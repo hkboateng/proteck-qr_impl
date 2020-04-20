2db48ffc1638e15cf75eb5019d785ba44bf549c8
Hubert Kyeremateng-Boateng <hkboateng@gmail.com>
2020-04-19 20:44:22 -0400

    initial commit

---
+++ src/main/java/com/protecksoftware/qr/ProteckQrServiceImplApplication.java
@@ -0,0 +1,13 @@
+package com.protecksoftware.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteckQrServiceImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteckQrServiceImplApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/protecksoftware/qr/controller/QRController.java
@@ -0,0 +1,5 @@
+package com.protecksoftware.qr.controller;
+
+public class QRController {
+
+}
